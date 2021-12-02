64d829466f2a4d2b964f8fbacd75b2b0812a6377
markskroba <43658779+markskroba@users.noreply.github.com>
2021-12-02 01:20:21 -0600

    Update server.c

--- server.c
+++ server.c
@@ -1,3 +1,4 @@
+#define _GNU_SOURCE
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <stdlib.h>
@@ -5,26 +6,24 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <stdbool.h>
+#include <errno.h>
 
 #include <netinet/in.h>
 
 #define PORT 2222 
 
-//#define PATH "/student/mskroba/course/csci3500/"
-#define PATH "/tmp/test3"
-
 int main() {
 	struct sockaddr_in address;
 
-	int sockfd = socket(AF_INET, SOCK_STREAM, 0);
+	int sockfd = socket(AF_INET, SOCK_STREAM | SOCK_NONBLOCK, 0);
 	if (sockfd == 0) {
 		perror("Error creating socket");
 		exit(-1);
 	}
 
 	address.sin_family = AF_INET;
-    address.sin_addr.s_addr = INADDR_ANY;
-    address.sin_port = htons( PORT );
+	address.sin_addr.s_addr = INADDR_ANY;
+	address.sin_port = htons( PORT );
 
 	int retval = bind(sockfd, (struct sockaddr *) &address, sizeof(address));
 	if (retval == -1) {
@@ -37,32 +36,33 @@ int main() {
 		perror("Error listening for clients");
 		exit(-1);
 	}	
+	int active_socket;
 
+	while (true) {
 
-	int socket_data = accept(sockfd, NULL, NULL);
-	if (socket_data == -1) {
-		perror("Error accepting a client");
+		char buffer[1024] = {0};
+		int retval = read(active_socket, buffer, sizeof(buffer));
+		if (retval == -1 && errno == EINVAL) {
+			perror("error reading");
+		}
+		else if (retval != -1) {
+			printf("message: '%s'\n", buffer);
+			return 0;
+		}
+
+		//adding new sockets
+		int new_socket = accept4(sockfd, NULL, NULL, SOCK_NONBLOCK);
+		if (new_socket == -1 && errno != EAGAIN) {
+			perror("error");
+		}
+		else if (new_socket != -1) {
+			printf("established new connection with socket %d\n", new_socket);
+			active_socket = new_socket;
+		}
+		
+		sleep(1);
 	}
 
-	// while (true) {
-	// 	char buffer [1024];
-	// 	read(socket_data, buffer, sizeof(buffer));
-	// 	buffer[sizeof(buffer) - 1] = 0;
-	// 	if (strncmp(buffer, "quit", sizeof(buffer))) {
-	// 		break;
-	// 	} 
-	// 	else {
-	// 		printf("%s\n", buffer);
-	// 	}
-
-	// 	close(socket_data);
-	// 	int socket_data = accept(sockfd, NULL, NULL);
-	// 	if (socket_data == -1) {
-	// 		perror("Error accepting a client");
-	// 	}
-	// }
-
 	close(sockfd);
-	unlink(PATH);
 	return 0;
 }
