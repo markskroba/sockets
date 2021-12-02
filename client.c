9c3ba069c1bd48c9d753464afa1aaa260d0ae81b
markskroba <43658779+markskroba@users.noreply.github.com>
2021-12-02 01:20:00 -0600

    Update client.c

--- client.c
+++ client.c
@@ -1,3 +1,4 @@
+#define _GNU_SOURCE
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <stdlib.h>
@@ -5,6 +6,7 @@
 #include <stdio.h>
 #include <sys/un.h>
 #include <unistd.h>
+#include <errno.h>
 
 #include <netinet/in.h>
 #include <arpa/inet.h>
@@ -16,7 +18,7 @@ int main() {
 	int sockfd;
 	struct sockaddr_in address;
 
-    sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    sockfd = socket(AF_INET, SOCK_STREAM | SOCK_NONBLOCK, 0);
     if (sockfd == 0) {
         perror("Error creating a socket");
         exit(-1);
@@ -27,10 +29,18 @@ int main() {
     address.sin_addr.s_addr = inet_addr("127.0.0.1");
 
     int retval = connect(sockfd, (struct sockaddr *) &address, sizeof(address));
-    if (retval == -1) {
+    if (retval == -1 && errno != EINPROGRESS) {
         perror("Error establishing a connection to server");
         exit(-1);
     }
+
+    printf("established connection %d\n", retval);
+
+    char buffer[1024] = {0};
+    sprintf(buffer, "%s", "test");
+    printf("message: %s, size: %d\n", buffer,sizeof(buffer));
+    write(sockfd, buffer, sizeof(buffer));
+    printf("message sent\n");
 
     return 0;
 }
